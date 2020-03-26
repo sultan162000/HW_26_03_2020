40982ede169191c9ae72a6eaffc7aa613ba98fd3
Кодиров Султон <sultonkodiro109@gmail.com>
2020-03-27 00:47:29 +0500

    started project

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace HW_26_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////Задача который решили в классе
+            /* 
+            decimal userBalance = 300m;
+            string basket = "";
+            start:
+            Console.WriteLine($"Ваш баланс - {userBalance}");
+            if(userBalance<=0){
+                Console.WriteLine("У вас недостаточно средств");
+                goto End;
+            }
+
+            Console.Write(""+
+            "1. Кола - 10 сомон\n"+
+            "2. Чипсы - 14 сомон\n"+
+            "3. M&Ms - 6 сомон\n"+
+            "4. Snikers - 7 сомон\n"+
+            "5. Орд - 190 сомон\n"+
+            "Выберите по индексу: "
+            );
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                {
+                    if(userBalance - 10 >= 0){
+                    basket += "Кола, ";
+                    userBalance -= 10;
+                    }else{
+                        Console.WriteLine("НЕдостаточно средст");
+                        goto End;
+                    }
+                    Console.Write("Вы хотите продолжите? д/н");
+                    if(Console.ReadLine().ToLower()=="д")goto start;
+                    else goto End;
+
+                }
+                case 2:
+                {
+                    if(userBalance - 14 >= 0){
+                    basket += "Чипсы, ";
+                    userBalance -= 14;
+                    }else{
+                        Console.WriteLine("НЕдостаточно средст");
+                        goto End;
+                    }
+                    Console.Write("Вы хотите продолжите? д/н");
+                    if(Console.ReadLine().ToLower()=="д")goto start;
+                    else goto End;
+
+                }
+                case 3:
+                {
+                    if(userBalance - 6 >= 0){
+                    basket += "M&Ms, ";
+                    userBalance -= 6;
+                    }else{
+                        Console.WriteLine("НЕдостаточно средст");
+                        goto End;
+                    }
+                    Console.Write("Вы хотите продолжите? д/н");
+                    if(Console.ReadLine().ToLower()=="д")goto start;
+                    else goto End;
+
+                }
+                case 4:
+                {
+                    if(userBalance - 7 >= 0){
+                    basket += "Snikers, ";
+                    userBalance -= 7;
+                    }else{
+                        Console.WriteLine("НЕдостаточно средст");
+                        goto End;
+                    }
+                    Console.Write("Вы хотите продолжите? д/н");
+                    if(Console.ReadLine().ToLower()=="д")goto start;
+                    else goto End;
+                }
+                case 5:
+                {
+                    if(userBalance - 190 >= 0){
+                    basket += "Орд, ";
+                    userBalance -= 190;
+                    }else{
+                        Console.WriteLine("НЕдостаточно средст");
+                        goto End;
+                    }
+                    Console.Write("Вы хотите продолжите? д/н: ");
+                    if(Console.ReadLine().ToLower()=="д")goto start;
+                    else goto End;
+                }
+                default:
+                goto start;
+                
+            }
+            
+            End:
+            Console.Write($"Ваша покупка: {basket}\nВаш баланс {userBalance}");
+            */
+
+            ///////////////Задание 2
+            
+            double userBalance = 54;
+            double disc = 0;
+            if(userBalance >= 500 && userBalance < 1000)
+            disc = userBalance * 3 / 100;
+            else if(userBalance >= 1000)
+            disc = userBalance * 5 /100;
+            else Console.WriteLine("В вашей сумме не полагается скидка.");
+
+            Console.WriteLine($"Ваша скидка {disc}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
